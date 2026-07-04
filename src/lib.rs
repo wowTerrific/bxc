710a52fee7f5d0420108ae3cef707eff6de8698f
wowterrific <marshallstutzman@gmail.com>
2026-07-04 09:47:20 -0400

    added binary input option - need to refactor

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,35 @@
-// main functionality may live here, depends on string/str conversions are helpful beyond this CLI.
-// I'm guessing it may not be.
+use std::error::Error;
 
+pub fn hex_to_int(num_str: &str) -> Result<u64, Box<dyn Error>> {
+    let mut bytes: Vec<u8> = String::from(num_str).into_bytes();
+    bytes.reverse();
+
+    let mut num: u64 = 0;
+    for (i, n) in bytes.iter().enumerate() {
+        let n = match n {
+            b'0'..=b'9' => n - b'0',
+            b'a'..=b'f' => n - b'a' + 10,
+            b'A'..=b'F' => n - b'A' + 10,
+            _ => return Err("Must supply valid hex input".into()),
+        };
+        num += (n as u64) * 16_u64.pow(i as u32);
+    }
+    Ok(num)
+}
+
+
+
+pub fn bin_to_int(num_str: &str) -> Result<u64, Box<dyn Error>> {
+    let mut bytes: Vec<u8> = String::from(num_str).into_bytes();
+    bytes.reverse();
+
+    let mut num: u64 = 0;
+    for (i, n) in bytes.iter().enumerate() {
+        match n {
+            b'0'..=b'1' => num += (n - b'0') as u64 * 2_u64.pow(i as u32),
+            _ => return Err("must supply valid binary input".into()),
+        }
+    }
+
+    Ok(num)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,21 @@
 use std::env;
 use std::error::Error;
 
-fn hex_to_int(num_str: &str) -> Result<u64, Box<dyn Error>> {
-    let mut bytes: Vec<u8> = String::from(num_str).into_bytes();
-    bytes.reverse();
-
-    let mut num: u64 = 0;
-    for (i, n) in bytes.iter().enumerate() {
-        let n = match n {
-            b'0'..=b'9' => n - b'0',
-            b'a'..=b'f' => n - b'a' + 10,
-            b'A'..=b'F' => n - b'A' + 10,
-            _ => return Err("Must supply valid hex input".into()),
-        };
-        num += (n as u64) * 16_u64.pow(i as u32);
-    }
-    Ok(num)
-}
+// maximum entry possible: u64::MAX;
+// 18446744073709551615
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut args = env::args();
     args.next(); // bxc
-    
 
     while let Some(arg) = args.next() {
         let arg = arg.to_lowercase();
         let num: u64;
 
-        // TODO: not taking into account binary input
         if arg.starts_with("0x") {
-            num = hex_to_int(&arg[2..])?;    
+            num = bxc::hex_to_int(&arg[2..])?;    
+        } else if arg.starts_with("0b") {
+            num = bxc::bin_to_int(&arg[2..])?;
         } else {
             num = arg.parse()?;
         }
@@ -39,7 +25,6 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
 
     Ok(())
-    
 }
 
 
